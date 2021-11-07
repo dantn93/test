a608d74eb765e33bbe5a9ae9b9b47b5fee69223e
Dan Tran <dantn93@gmail.com>
2021-11-07 22:43:47 +0700

    feat: demo commit line
    commit demo 3
    
    demo commit 4
    
    demo commit 7
    
    demo commit 8

--- demo.js
+++ demo.js
@@ -1,2 +1,8 @@
 console.log("Main commit 1");
 console.log("Main commit 2");
+
+console.log("Demo commit 3");
+console.log("Demo commit 4");
+
+console.log("Demo commit 7");
+console.log("Demo commit 8");
